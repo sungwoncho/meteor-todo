e4344d403dcacffe76a44b0614f9f4293440ef5a
Sung Won Cho <mikeswcho@gmail.com>
2015-05-16 12:51:43 +1000

    Initial commit with a template

---
+++ meteor-todos.js
@@ -0,0 +1,10 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Template.body.helpers({
+    tasks: [
+      { text: "This is task 1" },
+      { text: "This is task 2" },
+      { text: "This is task 3" }
+    ]
+  })
+}
