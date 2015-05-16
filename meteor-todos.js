2f026c887f968361b26906d7e7ff7355981893f0
Sung Won Cho <mikeswcho@gmail.com>
2015-05-16 13:30:37 +1000

    Add login system

--- meteor-todos.js
+++ meteor-todos.js
@@ -24,7 +24,9 @@ if (Meteor.isClient) {
 
       Tasks.insert({
         text: text,
-        createdAt: new Date()
+        createdAt: new Date(),
+        owner: Meteor.userId(),
+        username: Meteor.user().username
       });
 
       event.target.text.value = "";
@@ -45,4 +47,8 @@ if (Meteor.isClient) {
       Tasks.remove(this._id)
     }
   })
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
 }
