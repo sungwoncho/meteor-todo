2fe7f23614394dfecf48d20bd927c03fc1936d9a
Sung Won Cho <mikeswcho@gmail.com>
2015-05-16 13:09:33 +1000

    Add update and remove

--- meteor-todos.js
+++ meteor-todos.js
@@ -21,5 +21,14 @@ if (Meteor.isClient) {
 
       return false;
     }
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function () {
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id)
+    }
   })
 }
